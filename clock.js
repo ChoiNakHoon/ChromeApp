b75acd04f3a8ebc7d7a8ef3cbd59651f01d6acce
raccoon86 <onehub86@gmail.com>
2020-03-08 20:48:21 +0900

    시계 작동 함수
    setInterval 이용하여 시계 함수를 매 초 마다 시계 함수 실행

--- clock.js
+++ clock.js
@@ -1,6 +1,11 @@
+/* 
+    시계 함수
+    매초 마다 시계가 움직이는 기능
+*/
 const clockContainer = document.querySelector(".js-clock"),
     clockTitle = clockContainer.querySelector("h1");
 
+const RUN_SECONDS = 1000;
 function getTime() {
     const date = new Date();
     const minutes = date.getMinutes();
@@ -9,8 +14,10 @@ function getTime() {
 
     clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
 }
+
 function init() {
     getTime();
+    setInterval(getTime, RUN_SECONDS);
 }
 
 init();
