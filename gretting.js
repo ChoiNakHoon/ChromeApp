1fb94e2e7686bd7e9f202ed7f8cf0917148ef125
raccoon86 <onehub86@gmail.com>
2020-03-09 19:52:28 +0900

    로컬에 유저 네임 저장하기
     - form에 addEventListener를 붙여서 form에 문자를 입력하고 저장 했을 때 이벤트 발생
     - saveName 함수를 만들어 로컬에 저장 하도록 함

--- gretting.js
+++ gretting.js
@@ -5,6 +5,20 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveUserName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintName(currentValue);
+    saveUserName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 function paintName(text) {
     form.classList.remove(SHOWING_CN);
     gretting.classList.add(SHOWING_CN);
@@ -13,6 +27,7 @@ function paintName(text) {
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
+        askForName();
     } else {
         paintName(currentUser);
     }
